7da768d333dfb8e4859590490b7ce218889a9dc5
Patrick Lee Scott <pat@patscott.io>
2020-05-25 11:50:06 -0400

    fix: postgres operator sa namespace

--- get-postgres-operator-yaml.sh
+++ get-postgres-operator-yaml.sh
@@ -2,7 +2,8 @@
 
 git clone https://github.com/zalando/postgres-operator.git
 cd postgres-operator
-
+git fetch --all -p
+git checkout v1.5.0
 cp manifests/configmap.yaml ../env/templates/0_pgo_configmap.yaml
 cp manifests/operator-service-account-rbac.yaml ../env/templates/1_pgo_operator-service-account-rbac.yaml
 cp manifests/postgres-operator.yaml ../env/templates/2_pgo_postgres-operator.yaml
